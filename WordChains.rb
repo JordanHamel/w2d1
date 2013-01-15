b2538e6408f1928bebf4704891c3eae2080a0ba3
App Academy <app-academy@monet.att.net>
2013-01-14 16:29:30 -0800

    Included ArgumentErrors and Comments

--- WordChains.rb
+++ WordChains.rb
@@ -1,3 +1,6 @@
+# Ryan S and Kriti
+# W2D1
+
 class WordNode
   attr_reader :value, :parent
 
@@ -7,30 +10,45 @@ class WordNode
 end
 
 class WordChain
-
   def initialize(start_word, end_word)
     @start_word, @end_word = start_word, end_word
-    @dictionary = build_dictionary
+    @dictionary = File.readlines('dictionary.txt').map(&:chomp)
+
+    if !words_in_dictionary?
+      raise ArgumentError, "Use words in dictionary.txt"
+    elsif @start_word.length != @end_word.length
+      raise ArgumentError, "Please use words of the same length"
+    end
   end
 
   def find_path
     queue = [WordNode.new(@start_word, nil)]
+    initial_dictionary_filter
 
+    # Assigns word to be the first word in queue.
+    # If queue is empty, returns nil and breaks loop.
     while word = queue.shift
       break if word.value == @end_word
       children = find_children(word)
+
+      # Removes children from dictionary so that no word is checked
+      # again in a succeeding breadth.
       filter_dictionary(children)
+
       queue += children
     end
 
-    word.value == @end_word ? print_path(word) : puts("No path found.")
+    word.nil? ? puts("No path found.") : print_path(word)
   end
+
+  private
 
   def print_path(word)
     print_path(word.parent) unless word.parent.nil?
     puts word.value
   end
 
+  # Find words one letter away in the dictionary
   def find_children(word)
     children = []
     @dictionary.each do |child|
@@ -43,15 +61,19 @@ class WordChain
     children
   end
 
-  def build_dictionary
-    dictionary = File.readlines('dictionary.txt').map(&:chomp)
-    dictionary = dictionary.select { |word| word.length == @start_word.length }
-    dictionary.delete(@start_word)
-    dictionary
+  # Initial filter by word length and remove start_word
+  def initial_dictionary_filter
+    @dictionary.delete(@start_word)
+    @dictionary.select! { |word| word.length == @start_word.length }
   end
 
+  # Removes words from dictionary
   def filter_dictionary(words)
     words.each { |word| @dictionary.delete(word.value) }
+  end
+
+  def words_in_dictionary?
+    @dictionary.include?(@start_word) && @dictionary.include?(@end_word)
   end
 end
 
